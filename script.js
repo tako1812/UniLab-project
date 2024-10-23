63d6269246ef2467038223651648759ef7b18b2c
tako1812 <troba12@freeuni.edu.ge>
2024-10-23 16:59:26 +0400

    work on slider functionality

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
 'use strict';
+/********************/
+/*      Slider     */
+/******************/
+const slides = document.querySelectorAll('.slide-container');
+const btnLeft = document.querySelector('.btn-left');
+const btnRight = document.querySelector('.btn-right');
+
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const goToSlide = function (slide) {
+    slides.forEach(
+      (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+    );
+};
+
+const nextSlide = function () {
+    if (curSlide === maxSlide - 1) {
+      curSlide = 0;
+    } else {
+      curSlide++;
+    }
+    goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+    if (curSlide === 0) {
+      curSlide = maxSlide - 1;
+    } else {
+      curSlide--;
+    }
+    goToSlide(curSlide);
+};
+
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+
+
+
+
 
 /********************/
 /*   Display Map   */
